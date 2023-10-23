efb2e40a473327d48ee89043dcdbc66c6ff7c59c
SulienP <112190974+SulienP@users.noreply.github.com>
2023-10-23 22:38:35 +0200

    debut de la recuperation des employe

--- src/Home.go
+++ src/Home.go
@@ -1,6 +1,7 @@
 package sql
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -14,13 +15,20 @@ func Home(w http.ResponseWriter, r *http.Request) {
 tmpl := template.Must(template.ParseFiles("templates/Home.html"))
 
 	data := Employees{}
-	allEmploye := recuperationEmployee()
+
+	
 	if r.Method == http.MethodPost{
 		idFormulaireEmployee := r.FormValue("employee")
+		
 		idFormulaireManager := r.FormValue("employeeManager") 
 		idFormulaireHIre := r.FormValue("hire")
 		idFormulaireIncrease := r.FormValue("increase")
 	}
+		allEmploye := RecuperationEmployee(postid  ,  isPresent )
+	allManager := RecuperationManager( managerId, employeeId)
+	allPost := RecuperationPost( postId )
+	allDepartemnts := RecuperationDepartement(id )
+	fmt.Println(allEmploye, allManager, allPost, allDepartemnts)
 	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
---
+++ src/recuperation.go
@@ -0,0 +1,17 @@
+package sql
+
+func RecuperationEmployee(id int, isPresent bool) {
+
+}
+
+func RecuperationManager(managerId int, employeeId int) {
+
+}
+
+func RecuperationPost(id int) {
+
+}
+
+func RecuperationDepartement(id int) {
+
+}
