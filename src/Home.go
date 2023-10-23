ec964bd1a668631b7edd0ae6564201bb7df367d9
SulienP <112190974+SulienP@users.noreply.github.com>
2023-10-23 22:24:46 +0200

    ajout du début des structures pour récupéré les donneés

--- src/Home.go
+++ src/Home.go
@@ -13,10 +13,14 @@ import (
 func Home(w http.ResponseWriter, r *http.Request) {
 tmpl := template.Must(template.ParseFiles("templates/Home.html"))
 
-	data := Employees{
-		
+	data := Employees{}
+	allEmploye := recuperationEmployee()
+	if r.Method == http.MethodPost{
+		idFormulaireEmployee := r.FormValue("employee")
+		idFormulaireManager := r.FormValue("employeeManager") 
+		idFormulaireHIre := r.FormValue("hire")
+		idFormulaireIncrease := r.FormValue("increase")
 	}
-
 	err := tmpl.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
