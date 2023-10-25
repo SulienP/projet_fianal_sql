f4ee56a99ac570ada43b95cbe1599c2e8dc79be0
Harcadakad <evan.guillet@ynov.com>
2023-10-25 10:43:09 +0200

    Correction of problems during merge: completely empty dbQuery file

---
+++ src/DBQuery.go
@@ -0,0 +1,41 @@
+package sql
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func getDataBase() *sql.DB {
+	db, errSQLOpen := sql.Open("sqlite3", "public/compagny.db")
+	if errSQLOpen != nil {
+		fmt.Println(errSQLOpen)
+	}
+
+	return db
+}
+
+func getAllEmployees() []Employees {
+	db := getDataBase()
+
+	defer db.Close()
+
+	rows, errQuery := db.Query("SELECT * FROM employees")
+	if errQuery != nil {
+		log.Fatal(errQuery)
+	}
+	defer rows.Close()
+
+	var employees []Employees
+
+	for rows.Next() {
+		var employee Employees
+		err := rows.Scan(&employee.EmployeeId, &employee.PostId, &employee.FirstName, &employee.LastName, &employee.Email, &employee.Password, &employee.IsPresent, &employee.Salary, &employee.Schedule, &employee.BreackTimes, &employee.DateHire, &employee.EndContract)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		employees = append(employees, employee)
+	}
+	return employees
+}
