2ab527d094ef454d88848dcd4e7d1215dee62236
Harcadakad <evan.guillet@ynov.com>
2023-10-25 15:18:22 +0200

    Backup to change branch

--- src/DBQuery.go
+++ src/DBQuery.go
@@ -15,12 +15,37 @@ func getDataBase() *sql.DB {
 	return db
 }
 
-func getAllEmployees() []Employees {
+func getEmployees() []Employees {
 	db := getDataBase()
 
 	defer db.Close()
 
 	rows, errQuery := db.Query("SELECT * FROM employees")
+	if errQuery != nil {
+		log.Fatal(errQuery)
+	}
+	defer rows.Close()
+
+	var employees []Employees
+
+	for rows.Next() {
+		var employee Employees
+		err := rows.Scan(&employee.EmployeeId, &employee.PostId, &employee.FirstName, &employee.LastName, &employee.Email, &employee.Password, &employee.IsPresent, &employee.Salary, &employee.Schedule, &employee.BreackTimes, &employee.DateHire, &employee.EndContract)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		employees = append(employees, employee)
+	}
+	return employees
+}
+
+func getSubordinates(idManager string) []Employees {
+	db := getDataBase()
+
+	defer db.Close()
+
+	rows, errQuery := db.Query("SELECT firstName, lastName FROM employees INNER JOIN posts ON employees.postId = posts.postId WHERE posts.managerId = " + idManager)
 	if errQuery != nil {
 		log.Fatal(errQuery)
 	}
--- src/Home.go
+++ src/Home.go
@@ -1,7 +1,6 @@
 package sql
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"text/template"
@@ -11,20 +10,23 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/home.html"))
 
 	// Get the list of employees
-	employees := getAllEmployees()
+	//currentData := getAllEmployees()
+	currentData := getEmployees()
 
-	if r.Method == http.MethodPost {
-		idEmployee := r.FormValue("employee")
-		idManager := r.FormValue("employeeManager")
-		idHire := r.FormValue("hire")
-		idIncrease := r.FormValue("increase")
-		fmt.Println(idEmployee, idManager, idHire, idIncrease)
+	/*
+		if r.Method == http.MethodPost {
+			idEmployee := r.FormValue("employee")
+			idManager := r.FormValue("employeeManager")
+			idHire := r.FormValue("hire")
+			idIncrease := r.FormValue("increase")
+			fmt.Println(idEmployee, idManager, idHire, idIncrease)
 
-		// Handle form submissions here if needed
-	}
+			// Handle form submissions here if needed
+		}
+	*/
 
 	// Pass the list of employees to the template
-	err := tmpl.Execute(w, employees)
+	err := tmpl.Execute(w, currentData)
 	if err != nil {
 		log.Fatal(err)
 	}
